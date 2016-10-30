fcb34dffe04c05367da66e9e36ef41555f121f9c
Faraz <farazmr@gmail.com>
2016-10-31 01:24:37 +0530

    Initial Commit

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "GameOfLife.h"
+using namespace std;
+
+
+int main()
+{
+	int row, col;
+	cin >> row >> col;		//Read initial state's dimensions
+
+	GameOfLife gol(row, col);
+	gol.readInitialState();
+	gol.generateNextState();
+	gol.printState();
+
+	cin >> row;
+	return 0;
+}
